f12c2251cf635359e251f6649c167e2ccbea153d
Ida Kristin Hagen <idahagen@hotmail.com>
2015-10-19 00:54:42 +0200

    Dropdownmenu started

---
+++ GrasdalGlass/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrasdalGlass.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult Annet()
+        {
+            ViewBag.Message = "Your annet glass page";
+
+            return View();
+        }
+
+        public ActionResult Nybygg()
+        {
+            ViewBag.Message = "Your nybygg page.";
+
+            return View();
+        }
+
+        public ActionResult Proff()
+        {
+            ViewBag.Message = "Your proff page.";
+
+            return View();
+        }
+
+        public ActionResult Reparasjoner()
+        {
+            ViewBag.Message = "Your reparasjoner page.";
+
+            return View();
+        }
+
+        public ActionResult Vindu()
+        {
+            ViewBag.Message = "Your vindu page.";
+
+            return View();
+        }
+    }
+}
