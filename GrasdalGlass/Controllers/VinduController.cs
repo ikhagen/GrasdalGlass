ecd70fd851fad0e2bab26983d0a23b2a1c663bf1
Ida Kristin Hagen <idahagen@hotmail.com>
2015-10-19 14:06:58 +0200

    Functioning dropdown

---
+++ GrasdalGlass/Controllers/VinduController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrasdalGlass.Controllers
+{
+    public class VinduController : Controller
+    {
+        public ActionResult Energispareglass()
+        {
+            return View();
+        }
+
+        public ActionResult Selvrensende_glass()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Sikkerhetsglass_sikringsglass()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Solbeskyttende_glass()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Støydempende_glass()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
